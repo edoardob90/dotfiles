36e579c71810a5129060bdb141ad628cd13c135b
edoardob90 <edoardob90@gmail.com>
2018-09-20 11:45:58 +0200

    Fixed hard-coded home directory path

--- fzf.zsh
+++ fzf.zsh
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/ebaldi/.dotfiles/fzf/bin* ]]; then
-  export PATH="$PATH:/home/ebaldi/.dotfiles/fzf/bin"
+if [[ ! "$PATH" == *"$HOME"/.dotfiles/fzf/bin* ]]; then
+  export PATH="$PATH:$HOME/.dotfiles/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/ebaldi/.dotfiles/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${HOME}/.dotfiles/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/ebaldi/.dotfiles/fzf/shell/key-bindings.zsh"
+source "${HOME}/.dotfiles/fzf/shell/key-bindings.zsh"
 
