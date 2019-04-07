e6645a4e03987a90305d57d49083c749f151b2ea
edoardob90 <edoardob90@gmail.com>
2019-04-07 21:27:14 +0200

    Tidying up my dotfiles, plus switching to zpretzo

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for file in bash* profile zshrc inputrc; do
-    if [[ ! -f "$HOME/$file" ]]; then
-        ln -s ${PWD}/$(basename ${file}) ${HOME}/.${file}
-    else
-        echo "File ${file} already exists in home directory!" >&2
-    fi
-done
-exit 0
