2d766b5637d6a9d70b912b905bb56984310e9b9d
edoardob90 <edoardob90@gmail.com>
2020-09-22 13:19:33 +0200

    Replaces virtualenvwrapper with pyenv-virtualenv

--- zsh/env-macos.zsh
+++ zsh/env-macos.zsh
@@ -5,11 +5,12 @@
 export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 
 # Set up Python `virtualenvwrapper`
-export WORKON_HOME=~/.virtualenvs
-[[ ! -d "$WORKON_HOME" ]] && mkdir -p $WORKON_HOME
-if command -v pyenv 1>/dev/null 2>&1; then
-    source $(pyenv which virtualenvwrapper.sh)
-else
-    echo "WARNING: pyenv not found. Install with 'brew install pyenv' and set a global Python version" >&2
-fi
+# This MUST BE COMMENTED if pyenv-virtualenv plugin is installed
+#export WORKON_HOME=~/.virtualenvs
+#[[ ! -d "$WORKON_HOME" ]] && mkdir -p $WORKON_HOME
+#if command -v pyenv 1>/dev/null 2>&1; then
+#    source $(pyenv which virtualenvwrapper.sh)
+#else
+#    echo "WARNING: pyenv not found. Install with 'brew install pyenv' and set a global Python version" >&2
+#fi
 
