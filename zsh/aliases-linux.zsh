855747685be1b8d24cf1e9c403d99eb18821faf0
edoardob90 <edoardob90@gmail.com>
2020-10-16 12:36:07 +0200

    Better way of managing OS-specific aliases

--- zsh/aliases-linux.zsh
+++ zsh/aliases-linux.zsh
@@ -1,12 +1,4 @@
-# Custom aliases
-#
-# VIM/Neovim
-if type nvim 2>&1 >/dev/null; then
-    alias vi=nvim
-    alias vim=nvim
-else
-    alias vi=vim
-fi
+# Linux custom aliases
 
 # SSH shortcuts
 alias daint='ssh daint.cscs.ch'
@@ -15,23 +7,5 @@ alias daint='ssh daint.cscs.ch'
 alias ls='ls -G --color=auto'
 alias lls='ls -lart'
 
-# cat
-# Use 'bat' instead of 'cat' if installed
-# On macOS: brew install bat
-if type bat 2>&1 >/dev/null; then
-    alias cat=bat
-fi
-
-# History
-function hs {
-    history 0 | grep $*
-}
-alias h='history 0'
-alias hsi='hs -i'
-
 # Nord VPN
 alias vpn=nordvpn
-
-# Source a local file for aliases
-# This file SHOULD NEVER BE ADDED to the Git repo to avoid errors
-[ -f "$DOTDIR/zsh/aliases.local.zsh" ] && source $DOTDIR/zsh/aliases.local.zsh
--- zsh/aliases-macos.zsh
+++ zsh/aliases-macos.zsh
@@ -1,19 +1,4 @@
-# Custom alias files
-# This file should be either symlinked in ${HOME} or in ${DOTFILES}/zsh
-#
-# VIM
-if type nvim 2>&1 >/dev/null; then
-    alias vi=nvim
-    alias vim=nvim
-else
-    alias vi=vim
-fi
-
-# History
-function hs {
-    history 0 | grep $*
-}
-alias hsi='hs -i'
+# macOS custom aliases
 
 # SSH
 alias ssh-agent='/usr/bin/ssh-agent' # keep using macOS ssh agent even if openssh via Brew is installed
@@ -32,7 +17,3 @@ fi
 # Search running processes for a pattern
 alias 'ps?'="ps ax | grep -v grep | grep -iE"
 alias psgrep='psgrep -saien'
-
-# Source a local file for aliases
-# This file SHOULD NEVER BE ADDED to the Git repo to avoid errors
-[ -f "$DOTDIR/zsh/aliases.local.zsh" ] && source $DOTDIR/zsh/aliases.local.zsh
