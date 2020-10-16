127602d8fa7be6255fd1a731438ac1b9902bb39e
edoardob90 <edoardob90@gmail.com>
2020-10-16 11:50:42 +0200

    Proper setting of aliases and env variables

--- zsh/env-macos.zsh
+++ zsh/env-macos.zsh
@@ -2,15 +2,8 @@
 
 # Replace default macOS utils (e.g., grep, sed, awk) with GNU core utils
 # brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep
-export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/opt/gnu-sed/libexec/gnubin:${PATH}"
+export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
 
-# Set up Python `virtualenvwrapper`
-# This MUST BE COMMENTED if pyenv-virtualenv plugin is installed
-#export WORKON_HOME=~/.virtualenvs
-#[[ ! -d "$WORKON_HOME" ]] && mkdir -p $WORKON_HOME
-#if command -v pyenv 1>/dev/null 2>&1; then
-#    source $(pyenv which virtualenvwrapper.sh)
-#else
-#    echo "WARNING: pyenv not found. Install with 'brew install pyenv' and set a global Python version" >&2
-#fi
-
+# Make sure that ~/bin is *always* searched first
+export PATH=${HOME}/bin:${PATH}
