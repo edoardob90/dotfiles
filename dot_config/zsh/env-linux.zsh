eb9f5e2eff6d8ac79f2e3b4c1b9df205a2ecc8d4
edoardob90 <edoardob90@gmail.com>
2021-09-23 18:41:45 +0200

    Brew on Linux

--- dot_config/zsh/env-linux.zsh
+++ dot_config/zsh/env-linux.zsh
@@ -1,5 +1,11 @@
 # Environment file for Linux distributions
 
+# Setup Linuxbrew
+BREW="/home/linuxbrew/.linuxbrew/bin/brew"
+if type -a "$BREW" 2>&1 >/dev/null; then
+    eval "$("$BREW" shellenv)"
+fi
+
 # NVIDIA Cuda
 export PATH=/usr/local/cuda/bin:/usr/local/cuda/nsight-systems-2019.5.2${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
