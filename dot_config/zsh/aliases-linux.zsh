205031c0fc284554e47f8911d0732317edd4a184
edoardob90 <edoardob90@gmail.com>
2022-02-06 15:56:10 +0100

    Add aliases on Linux

--- dot_config/zsh/aliases-linux.zsh
+++ dot_config/zsh/aliases-linux.zsh
@@ -2,10 +2,19 @@
 
 # SSH shortcuts
 alias daint='ssh daint.cscs.ch'
+# When using kitty terminal emulator, alias ssh to have terminfo file available on the server
+if [[ "$TERM" == "xterm-kitty" ]]; then
+    alias ssh='kitty +kitten ssh'
+fi
 
 # ls
 alias ls='ls -G --color=auto'
 alias lls='ls -lart'
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
+alias lrt='ls -lrth'
 
 # Nord VPN
 alias vpn=nordvpn
