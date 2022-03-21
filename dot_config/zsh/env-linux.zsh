421d067e2d059f5756e83a47c95b814abf4236be
edoardob90 <edoardob90@gmail.com>
2022-03-21 15:26:41 +0100

    Linux: commands to enable/disable Brew

--- dot_config/zsh/env-linux.zsh
+++ dot_config/zsh/env-linux.zsh
@@ -1,11 +1,21 @@
 # Environment file for Linux distributions
 
 # Setup Linuxbrew
-BREW="/home/linuxbrew/.linuxbrew/bin/brew"
-if type -a "$BREW" 2>&1 >/dev/null; then
-    eval "$("$BREW" shellenv)"
-fi
+function brew_disable() {
+    export PATH=${PATH##*"/.linuxbrew/bin:"}
+    export PATH=${PATH##*"/.linuxbrew/sbin:"}
+    export MANPATH=${MANPATH##*"/.linuxbrew/share/man:"}
+    export INFOPATH=${INFOPATH##*"/.linuxbrew/share/info:"}
+}
+function brew_enable() {
+    BREW='/home/linuxbrew/.linuxbrew'
+    brew_disable
+    export PATH="$BREW/bin:$BREW/sbin:$PATH"
+    export MANPATH="$BREW/share/man:$MANPATH"
+    export INFOPATH="$BREW/share/info:$INFOPATH"
+    export HOMEBREW_NO_AUTO_UPDATE=1
+}
+brew_enable
 
-# NVIDIA Cuda
-export PATH=/usr/local/cuda/bin:/usr/local/cuda/nsight-systems-2019.5.2${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+# McFly
+eval "$($(brew --prefix)/bin/mcfly init zsh)"
