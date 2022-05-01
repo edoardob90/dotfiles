fd5dc07e5158b9fcbe91c7fb0e88df3e58589c91
edoardob90 <edoardob90@gmail.com>
2022-05-01 16:36:40 +0200

    Add Fish shell config

---
+++ dot_config/private_fish/conf.d/abbrevs.fish
@@ -0,0 +1,21 @@
+# Fish-equivalent for ZSH abbreviations
+abbr --add --global "ga"    git add
+abbr --add --global "gco"   git commit -a
+abbr --add --global "gca"    git commit -A
+abbr --add --global "gcom"  git commit -m
+abbr --add --global "gchm"  git checkout master
+abbr --add --global "gche"  git checkout
+abbr --add --global "gri"   git rebase -i HEAD~
+abbr --add --global "grbm"  git rebase origin/master
+abbr --add --global "gst"   git status
+abbr --add --global "gs"    git status -bs
+abbr --add --global "gps"   git push
+abbr --add --global "gpu"   git pull
+abbr --add --global "gfe"   git fetch
+abbr --add --global "gfm"   git fetch origin/master
+abbr --add --global "grv"   git remote -v
+abbr --add --global "glog"  git log
+abbr --add --global "grlog" git reflog
+abbr --add --global "gbra"  git branch -a
+abbr --add --global "grm"   git rm
+
---
+++ dot_config/private_fish/conf.d/aliases.fish
@@ -0,0 +1,50 @@
+# Aliases & functions
+# history
+function hs -d "Search through history"
+    history | grep $argv
+end
+function hsi
+    hs -i $argv
+end
+
+# macOS custom aliases
+alias tmp='cd ~/tmp'
+alias pypy='pypy3'
+alias vlc='/Applications/VLC.app/Contents/MacOS/VLC'
+alias ia='open -b pro.writer.mac -n'
+alias marked='open -b com.brettterpstra.marked2'
+alias screen='/usr/local/bin/screen'
+alias wls='wolframscript'
+
+# VIM/Neovim
+if type nvim 2>&1 >/dev/null
+    abbr --add --global vim nvim
+    alias vi=nvim
+else
+    alias vi=vim
+end
+
+# ls
+alias ls='lsd'
+alias l='ls -l'
+alias la='ls -a'
+alias lla='ls -la'
+alias lt='ls --tree'
+alias lrt='ls -lrth'
+
+# SSH
+alias ssh-agent='/usr/bin/ssh-agent' # keep using macOS ssh agent even if openssh via Brew is installed
+alias ssh-add='/usr/bin/ssh-add'
+alias daint='ssh daint.cscs.ch'
+alias router='/usr/bin/ssh router'
+if test "$TERM" = "xterm-kitty"
+    function ssh -d "ssh if using Kitty terminal"
+        kitty +kitten ssh $argv
+    end
+end
+
+# Misc
+alias cat='bat'
+function psq -d "Query running processes"
+    ps ax | grep -v grep | grep -iE $argv
+end
