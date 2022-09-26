f0b5f0b3cb5201562b2477d8be7ca2c631aec101
edoardob90 <edoardob90@gmail.com>
2022-09-26 13:13:41 +0200

    Fish shell config files

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    # zoxide: smarter 'cd' command
+    zoxide init fish | source
+    # Pyenv
+    pyenv init - | source
 end
