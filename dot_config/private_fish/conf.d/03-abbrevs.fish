f9939a3374b7f4fee225b8a1448a14a23adfaa85
Edoardo Baldi <edoardob90@gmail.com>
2024-10-12 18:24:44 +0200

    Update fish abbrevs

--- dot_config/private_fish/conf.d/03-abbrevs.fish
+++ dot_config/private_fish/conf.d/03-abbrevs.fish
@@ -1,14 +1,14 @@
 # Fish-equivalent for ZSH abbreviations
 abbr --add --global "ga"    git add
 abbr --add --global "gco"   git commit -a
-abbr --add --global "gca"   git commit -A
+# abbr --add --global "gca"   git commit -A
 abbr --add --global "gcom"  git commit -m
-abbr --add --global "gchm"  git checkout main
+abbr --add --global "gckm"  git checkout main
 abbr --add --global "gck"   git checkout
 abbr --add --global "gri"   git rebase -i HEAD~
 abbr --add --global "grbm"  git rebase origin/main
 abbr --add --global "gst"   git status
-abbr --add --global "gs"    git status -bs
+abbr --add --global "gsb"   git status -bs
 abbr --add --global "gps"   git push
 abbr --add --global "gpu"   git pull
 abbr --add --global "gfe"   git fetch
@@ -18,5 +18,9 @@ abbr --add --global "gl"    "git log --pretty=format:'%h %ad | %s%d [%an]' --gra
 abbr --add --global "grl"   git reflog
 abbr --add --global "gbra"  git branch -a
 abbr --add --global "grm"   git rm
-abbr --add --global "gsh"   git stash list
+abbr --add --global "gs"    git stash
+abbr --add --global "gsl"   git stash list
+abbr --add --global "gsp"   git stash pop
+abbr --add --global "gre"   git restore
+abbr --add --global "grs"   git restore --staged
 
